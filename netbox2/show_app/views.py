d51e2d8d1e3a2ff017f73072ee35c3cb382727e3
mahansepanloo <mahansepanloooo@gmail.com>
2025-05-18 08:52:07 +0330

    update

--- netbox2/show_app/views.py
+++ netbox2/show_app/views.py
@@ -6,7 +6,10 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django.utils.decorators import method_decorator
 
 
-class BaseContentViewSet(viewsets.ReadOnlyModelViewSet):
+class MovieViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Movie.objects.prefetch_related("genres", "directors", "actors").all()
+    serializer_class = MovieSerializer
+    lookup_field = "source_id"
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["title", "genres__name"]
     ordering_fields = ["release_year", "imdb"]
@@ -20,13 +23,18 @@ class BaseContentViewSet(viewsets.ReadOnlyModelViewSet):
         return super().retrieve(request, *args, **kwargs)
 
 
-class MovieViewSet(BaseContentViewSet):
-    queryset = Movie.objects.prefetch_related("genres", "directors", "actors")
-    serializer_class = MovieSerializer
-    lookup_field = "source_id"
-
-
-class SeriesViewSet(BaseContentViewSet):
-    queryset = Series.objects.prefetch_related("genres", "directors", "actors")
+class SeriesViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Series.objects.prefetch_related("genres", "directors", "actors").all()
     serializer_class = SeriesSerializer
     lookup_field = "source_id"
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["title", "genres__name"]
+    ordering_fields = ["release_year", "imdb"]
+
+    @method_decorator(cache_page(60 * 15))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 15))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
