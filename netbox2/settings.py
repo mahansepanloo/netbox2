c476c744d9a3bcf2c5435d796db32dd48e20073c
mahansepanloo <mahansepanloooo@gmail.com>
2025-05-18 12:16:12 +0330

    .

--- netbox2/settings.py
+++ netbox2/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'netbox2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netbox',
+        'NAME': 'NETBOX2',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
