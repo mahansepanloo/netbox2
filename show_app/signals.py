3665aa0425098d53c5127ee4a9600c552e4d9ea5
mahansepanloo <mahansepanloooo@gmail.com>
2025-05-18 17:24:23 +0330

    .

--- show_app/signals.py
+++ show_app/signals.py
@@ -1,18 +1,14 @@
+from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.core.cache import cache
 from .models import Movie, Series
 
-
-@receiver(post_save, sender=Movie)
-@receiver(post_delete, sender=Movie)
-def clear_movie_cache(sender, instance, **kwargs):
+@receiver([post_save, post_delete], sender=Movie)
+def clear_movie_cache(sender, **kwargs):
     cache.delete_pattern("movie_list*")
-    cache.delete_pattern(f"movie_detail*/{instance.source_id}*")
+    cache.delete_pattern("movie_detail*")
 
-
-@receiver(post_save, sender=Series)
-@receiver(post_delete, sender=Series)
-def clear_series_cache(sender, instance, **kwargs):
+@receiver([post_save, post_delete], sender=Series)
+def clear_series_cache(sender, **kwargs):
     cache.delete_pattern("series_list*")
-    cache.delete_pattern(f"series_detail*/{instance.source_id}*")
+    cache.delete_pattern("series_detail*")
